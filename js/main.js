939166138ec630438bea4e19e20c81f18ef8eeb8
hellingsstefan <hellingsstefan@gmail.com>
2013-03-25 14:18:07 +0100

    following tutorial on http://coenraets.org

--- js/main.js
+++ js/main.js
@@ -1,37 +1,20 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
 
-    initialize: function() {
-        this.store = new LocalStorageStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+	initialize: function() {
+		var self = this;
+		this.store = new MemoryStore(function() {
+			$('body').html(new HomeView(self.store).render().el);
+		});
+	}
 
 };
 
 app.initialize();
-
-jQuery(document).ready(function($) {
-	// SHOW BODY
-	$('body').fadeIn('slow');
-
-	// FadeIn Page Switch
-	$('a').click(function(e){
-		e.preventDefault();
-		var loc = $(this).attr('href');
-		$('body').fadeOut('slow', function(){
-			window.location = loc;
-		});
-	});
-});
