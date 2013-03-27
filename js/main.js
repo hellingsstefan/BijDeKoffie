da3d23627b7f41f99f3ff502b8de77696478164d
hellingsstefan <hellingsstefan@gmail.com>
2013-03-27 14:49:53 +0100

    style changes and backbutton script added

--- js/main.js
+++ js/main.js
@@ -88,7 +88,13 @@ $(document).ready(function() {
 	});
 });
 
-
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	document.addEventListener("backbutton", onBackKeyDown, false);
+}
+function onBackKeyDown() {
+	navigator.app.exitApp();
+}
 
 
 
